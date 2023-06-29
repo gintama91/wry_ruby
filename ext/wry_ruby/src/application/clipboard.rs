2f5088023157be69e3b7187b3e9c245a36e17e44
gintama91 <b9-20@srkrec.edu.in>
2023-06-29 21:31:26 +0530

    clipboard
    add rs doc link

---
+++ ext/wry_ruby/src/application/clipboard.rs
@@ -0,0 +1,77 @@
+// https://docs.rs/wry/latest/wry/application/clipboard/struct.Clipboard.html
+
+use std::cell::RefCell;
+
+use gdk;
+use gtk;
+
+use magnus::{Error, function, Object, Module, method};
+
+use wry::application::clipboard::Clipboard as ClipboardImpl;
+
+#[derive(Clone, Debug)]
+#[magnus::wrap(class = "Clip")]
+pub struct Clipboard {
+    inner: RefCell<ClipboardImpl>,
+}
+
+impl Clipboard {
+    pub fn new() -> Result<Self, Error> {
+        let inner = ClipboardImpl::new();
+
+        Ok(Self {
+            inner: RefCell::new(inner),
+        })
+    }
+
+    pub fn get(&self) -> std::cell::Ref<ClipboardImpl> {
+        self.inner.borrow()
+    }
+
+    pub fn get_mut(&self) -> std::cell::RefMut<ClipboardImpl> {
+        self.inner.borrow_mut()
+    }
+
+    /// Writes text to the clipboard.
+    ///
+    /// # Arguments
+    ///
+    /// * `s` - A string containing the text to be written.
+    ///
+    /// # Equivalent Ruby Method
+    ///
+    /// `clip.write_text("Hello World")`
+    pub fn write_text(&self, s: String) {
+        let text: &str = s.as_ref();
+        self.get_mut().write_text(text);
+    }
+
+    /// Reads text from the clipboard.
+    ///
+    /// # Equivalent Ruby Method
+    ///
+    /// `clip.read_text`
+    pub fn read_text(&self) -> Option<String> {
+        self.get().read_text()
+    }
+}
+
+/// Initializes the clipboard module.
+///
+/// # Equivalent Ruby Methods
+///
+/// - `clip = Clip.new`
+/// - `clip.write_text("Hello World")`
+/// - `clip.read_text`
+pub fn init() -> Result<(), Error> {
+    gdk::init();
+    let _ = gtk::init();
+
+    let class = magnus::define_class("Clip", Default::default())?;
+
+    class.define_singleton_method("new", function!(Clipboard::new, 0))?;
+    class.define_method("write_text", method!(Clipboard::write_text, 1))?;
+    class.define_method("read_text", method!(Clipboard::read_text, 0))?;
+
+    Ok(())
+}
