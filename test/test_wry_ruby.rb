d3dcb9458b07ca9b04fd3a66af06240773dadad8
gintama91 <pavannambi0408@gmail.com>
2023-07-19 10:29:25 +0530

    remove ci test for window as of now

--- test/test_wry_ruby.rb
+++ test/test_wry_ruby.rb
@@ -5,8 +5,8 @@ class TestWryRuby < Minitest::Test
   #   refute_nil ::WryRuby::VERSION
   # end
 
-  def test_hello_wry
-    # title, width, height, resizable , timeout (seconds) to close window during test
-    new_window "Hello Wry", 100, 400, true, 3
-  end
+  # def test_hello_wry
+  #   # title, width, height, resizable , timeout (seconds) to close window during test
+  #   new_window "Hello Wry", 100, 400, true, 3
+  # end
 end
