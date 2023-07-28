27df403cad5cc0df936d760c0071bed440fb9b47
gintama91 <pavannambi0408@gmail.com>
2023-07-28 13:39:29 +0530

    use html to load window
    messed up with filename ok
    
    don't do ci now

--- ext/wry_ruby/src/application/window.rs
+++ ext/wry_ruby/src/application/window.rs
@@ -1,10 +1,11 @@
-use magnus::{Error, define_global_function, function};
+use magnus::{Error, define_global_function, function, method};
 use wry::application::dpi::LogicalSize;
 use wry::application::window::WindowBuilder;
 use wry::application::{
     event::{Event, StartCause, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
 };
+use wry::webview::WebViewBuilder;
 use std::time::{Duration, Instant};
 
 pub fn WindoWnew(title: String, width: u32, height: u32, resizable: bool,timeout: u64) {
@@ -49,9 +50,37 @@ pub fn WindoWnew(title: String, width: u32, height: u32, resizable: bool,timeout
     });
 }
 
+pub fn window_with_html(html:String){
+     let event_loop = EventLoop::new();
+  let window = WindowBuilder::new()
+    .with_title("Shoes with html")
+    .build(&event_loop)
+    .unwrap();
+  let _webview = WebViewBuilder::new(window).unwrap()
+    .with_html(&html)
+    .unwrap()
+    .build()
+    .unwrap();
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
+
+}
 pub fn init() -> Result<(), Error> {
     println!("inside init");
     define_global_function("new_window", function!(WindoWnew, 5));
+    // as we use html in webiew i am trying to use it here not rly sure if it works though
+    define_global_function("window_with_html",function!(window_with_html,1));
 
     Ok(())
 }
--- test/test_wry_ruby.rb
+++ test/test_wry_ruby.rb
@@ -5,8 +5,15 @@ class TestWryRuby < Minitest::Test
   #   refute_nil ::WryRuby::VERSION
   # end
 
-  # def test_hello_wry
-  #   # title, width, height, resizable , timeout (seconds) to close window during test
-  #   new_window "Hello Wry", 100, 400, true, 3
-  # end
+  def test_hello_wry
+    # title, width, height, resizable, timeout (seconds) to close window during test
+    new_window "Hello Wry", 100, 400, true, 3
+
+  end
+
+  def use_html
+    html_content = File.read("test/shoes.html")  # Read the HTML content from your file hm maybee we can do this in rust side..???
+    window_with_html(html_content)
+  end
+
 end
